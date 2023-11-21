805ebb094402def1f43e2bd8d41b9bd1f4a8158c
Johann <johnhansong@gmail.com>
2023-11-20 18:47:03 -0800

    authenticate-phase-1-and-2

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const csurf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
+const { ValidationError } = require('sequelize'); // for sequelize errors
 
 // checking environment variable in .config/index.js file
 const { environment } = require('./config')
@@ -53,5 +54,44 @@ app.use(
 
 // connect all the routes
 app.use(routes);
+
+
+// Error handlers
+// catching all non-specific errors
+app.use((_req, _res, next) => {
+    // we are not using { _req, _res } in this callback function
+    const err = new Error("The requested resource couldn't be found.");
+    err.title = "Resource Not Found";
+    err.errors = { message: "The requested resource couldn't be found." };
+    err.status = 404;
+    next(err);
+});
+
+
+
+app.use((err, _req, _res, next) => {
+    // check if error is a Sequelize error:
+    if (err instanceof ValidationError) {
+        let errors = {};
+        for (let error of err.errors) {
+        errors[error.path] = error.message;
+    }
+        err.title = 'Validation error';
+        err.errors = errors;
+    }
+    next(err);
+});
+
+// Error formatter. Sends the response back to the client
+app.use((err, _req, res, _next) => {
+    res.status(err.status || 500);
+    console.error(err); //eventually remove
+    res.json({
+        title: err.title || 'Server Error',
+        message: err.message,
+        errors: err.errors,
+        stack: isProduction ? null : err.stack
+    });
+});
 
 module.exports = app;
