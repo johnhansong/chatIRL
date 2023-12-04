be547b2c96ff14caa2c3c79ab87901913915ee5c
Johann <johnhansong@gmail.com>
2023-12-03 23:45:41 -0800

    seeders-empty

---
+++ backend/db/seeders/20231204065003-groups.js
@@ -0,0 +1,23 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+
+// const { group } = require('../models')
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
