91dc03adf7c3d81f5fa5d3359d3bbcbd707be6f2
Johann <johnhansong@gmail.com>
2024-03-04 17:19:37 -0800

    Attendances-migration-debug

--- backend/db/models/attendance.js
+++ backend/db/models/attendance.js
@@ -11,8 +11,8 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      Attendance.belongsTo(models.User, { foreignKey: 'userId' })
-      Attendance.belongsTo(models.Event, { foreignKey: 'eventId' })
+      Attendance.belongsTo(models.User, { foreignKey: 'userId', onDelete: 'CASCADE' })
+      Attendance.belongsTo(models.Event, { foreignKey: 'eventId', onDelete: 'CASCADE'})
     }
   }
   Attendance.init({
