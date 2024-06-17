6c528c9bd7602103f51f78b4d96ddab079257a10
Johann <johnhansong@gmail.com>
2024-06-17 15:23:11 -0700

    render debug

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,13 +4,13 @@ import eslint from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  // plugins: [
-  //   react(),
-  //   eslint({
-  //     lintOnStart: true,
-  //     failOnError: mode === "production"
-  //   })
-  // ],
+  plugins: [
+    react(),
+    eslint({
+      lintOnStart: true,
+      failOnError: mode === "production"
+    })
+  ],
   // To automatically open the app in the browser whenever the server starts,
   // uncomment the following lines:
   server: {
